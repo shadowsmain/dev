f8713bd7b4865b679c385819957ca26ecd2c937c
Vladislav Teplov <61516555+shadowsmain@users.noreply.github.com>
2021-06-07 11:22:52 +0500

    Add files via upload

---
+++ student.py
@@ -0,0 +1,14 @@
+#from random import choice
+#books = [' ya robot', 'lunnaya pil', 'tainstvenniy ostrov']
+
+#for i in books:
+#    print(i)
+
+class Student:
+
+    name = "Vladislav"
+    birthday = "20.01.2002"
+    adress = "bluxera 10"
+
+    def start(self):
+        print("Студент 33 группы")
