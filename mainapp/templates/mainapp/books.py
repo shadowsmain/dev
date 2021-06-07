11ff51561a46560e42edf4334e0a8ef9840e40ce
vldln-biven'мамы.net <strifee322@gmail.com>
2021-06-07 11:19:17 +0500

    data

---
+++ mainapp/templates/mainapp/books.py
@@ -0,0 +1,11 @@
+#from random import choice
+#books = [' ya robot', 'lunnaya pil', 'tainstvenniy ostrov']
+
+#for i in books:
+#    print(i)
+
+class Student:
+
+    name = "Vladislav"
+    birthday = "20.01.2002"
+    adress = "bluxera 10"
